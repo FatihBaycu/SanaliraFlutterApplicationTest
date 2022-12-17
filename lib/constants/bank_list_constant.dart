760106309d2e359b493b76384829277c5e2b9e18
FatihBaycu <email@example.com>
2022-12-17 17:17:02 +0300

    bank image list constant

--- lib/constants/bank_list_constant.dart
+++ lib/constants/bank_list_constant.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../models/bank_image.dart';
+
 class BankListConstant{
 
 //Colors
@@ -14,6 +16,14 @@ static String turkBayragi="assets/icons/turk_bayragi.svg";
 static String bell="assets/icons/app_bar_icons/bell.svg";
 static String greenCircle="assets/icons/app_bar_icons/green_circle.svg";
 static String settings="assets/icons/app_bar_icons/settings.svg";
+static String ziraatBank="assets/banks_logos/ziraat_bankasi_logo.png";
+static String akbank= "assets/banks_logos/akbank_logo.png";
+static String albarakaBank= "assets/banks_logos/albaraka_logo.png";
+static String finansBank= "assets/banks_logos/finans_bank_logo.png";
+static String garantiBank="assets/banks_logos/garanti_BBVA_logo.png";
+static String kuveytTurkBank="assets/banks_logos/kuveyt_turk_logo.png";
+static String vakifBank= "assets/banks_logos/vakifbank_logo.png";
+static String banner= "assets/banner.jpg";
 
 //StringValues
 
@@ -23,5 +33,16 @@ static String money="234 ₺";
 static String selectBank="Türk Lirası yatırmak için banka seçiniz.";
 static String sendMoney="Havale / EFT ile para gönderin";
 
+
+static List<BankImage> bankImage=[
+  BankImage("T.C. ZİRAAT BANKASI A.Ş.", BankListConstant.ziraatBank),
+  BankImage("T.C. ZİRAAT BANKASI A.Ş.", BankListConstant.ziraatBank),
+  BankImage("AKBANK T.A.Ş.", BankListConstant.akbank),
+  BankImage("ALBARAKA TÜRK KATILIM BANKASI A.Ş.", BankListConstant.albarakaBank),
+  BankImage("QNB FİNANSBANK A.Ş.",BankListConstant.finansBank),
+  BankImage("TÜRKİYE GARANTİ BANKASI A.Ş.",BankListConstant.garantiBank),
+  BankImage("KUVEYT TÜRK KATILIM BANKASI A.Ş.", BankListConstant.kuveytTurkBank),
+  BankImage("TÜRKİYE VAKIFLAR BANKASI T.A.O.", BankListConstant.vakifBank),
+    ];
 
 }
--- lib/core/methods/get_bank_image.dart
+++ lib/core/methods/get_bank_image.dart
@@ -1,20 +1,10 @@
-    import '../../models/bank_image.dart';
+import 'package:sanalira_flutter_application_test/constants/bank_list_constant.dart';
 
-String getBankImage(String bankName){
-
-    List<BankImage> bankImage=<BankImage>[];
-    bankImage.add( BankImage("T.C. ZİRAAT BANKASI A.Ş.", "assets/banks_logos/ziraat_bankasi_logo.png"));
-    bankImage.add( BankImage("AKBANK T.A.Ş.", "assets/banks_logos/akbank_logo.png"));
-    bankImage.add( BankImage("ALBARAKA TÜRK KATILIM BANKASI A.Ş.", "assets/banks_logos/albaraka_logo.png"));
-    bankImage.add( BankImage("QNB FİNANSBANK A.Ş.", "assets/banks_logos/finans_bank_logo.png"));
-    bankImage.add( BankImage("TÜRKİYE GARANTİ BANKASI A.Ş.", "assets/banks_logos/garanti_BBVA_logo.png"));
-    bankImage.add( BankImage("KUVEYT TÜRK KATILIM BANKASI A.Ş.", "assets/banks_logos/kuveyt_turk_logo.png"));
-    bankImage.add( BankImage("TÜRKİYE VAKIFLAR BANKASI T.A.O.", "assets/banks_logos/vakifbank_logo.png"));
-
-    for(int i=0; i<bankImage.length; i++){
-      if(bankImage[i].bankName!.contains(bankName)){
-          return bankImage[i].bankImage.toString();
-      }
+String getBankImage(String bankName) {
+  for (int i = 0; i < BankListConstant.bankImage.length; i++) {
+    if (BankListConstant.bankImage[i].bankName!.contains(bankName)) {
+      return BankListConstant.bankImage[i].bankImage.toString();
     }
-    return "assets/banner.jpg";
   }
+  return BankListConstant.banner;
+}
